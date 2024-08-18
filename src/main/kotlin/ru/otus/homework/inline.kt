d7cc264cfd990d2b0f3b63d722ef0964a95e94c7
Motorro <motorro@gmail.com>
2024-08-18 11:49:50 +0200

    Non-inline lambda
    (cherry picked from commit 7fe7f86e43a201dddd2d59265d84934b3671d6a0)

---
+++ src/main/kotlin/ru/otus/homework/inline.kt
@@ -0,0 +1,14 @@
+package ru.otus.homework
+
+fun main() {
+    val a = 5
+    val result = funWithLambda(10) {
+        it * a
+    }
+    println(result)
+}
+
+fun funWithLambda(value: Int, action: (Int) -> Int): Int {
+    println("Passed value: $value")
+    return action(value)
+}
