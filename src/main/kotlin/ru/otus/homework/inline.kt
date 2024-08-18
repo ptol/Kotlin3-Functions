9c7bff712bb1ddafac4f8274e12c9b3d9e76222d
Motorro <motorro@gmail.com>
2024-08-18 13:21:31 +0200

    Non addressable code
    (cherry picked from commit b22cbe8bb7f753a3623738b6a94698b704ad7f5f)

--- src/main/kotlin/ru/otus/homework/inline.kt
+++ src/main/kotlin/ru/otus/homework/inline.kt
@@ -2,15 +2,25 @@ package ru.otus.homework
 
 fun main() {
     val a = 5
-    val result = funWithLambda(10) {
-        return
-    }
+    val result = funWithLambda(
+        value = 10,
+        action = { it * 10 },
+        onError = { it.message ?: "UNKNOWN"}
+    )
     println(result)
 }
 
-inline fun funWithLambda(value: Int, crossinline action: (Int) -> Int) {
+inline fun funWithLambda(value: Int, crossinline action: (Int) -> Int, onError: (Throwable) -> String) {
     println("Passed value: $value")
-    resultProcessor { action(value) }
+    try {
+        resultProcessor { action(value) }
+    } catch (e: Throwable) {
+        errorProcessor(e, onError)
+    }
+}
+
+fun errorProcessor(error: Throwable, block: (Throwable) -> String) {
+    println("Error: ${block(error)}")
 }
 
 fun resultProcessor(block: () -> Int) {
